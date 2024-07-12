6cbcc29c0aa3df3c191ac5a31f70e1f9afb2cb0e
yyy <1243933010@qq.com>
2024-07-12 20:41:10 +0800

    feat():添加注册

--- pages/login/register.vue
+++ pages/login/register.vue
@@ -8,32 +8,55 @@
 				<text>{{$t("app.name16")}}</text>
 			</view>
 			<view class="label">
-				<text>{{$t("app.name17")}}</text>
+				<text>{{$t("app.name27")}}</text>
 			</view>
 		</view>
 		<view class="form">
 			<view class="input">
-				<input type="text" :placeholder="$t('app.name12')" />
+				<input type="text" :placeholder="$t('app.name28')" />
 			</view>
 			<view class="input">
-				<input type="text" :placeholder="$t('app.name13')" />
+				<input type="text" :placeholder="$t('app.name29')" />
 			</view>
 			<view class="input">
-				<input type="text" :placeholder="$t('app.name13')" />
+				<input type="text" :placeholder="$t('app.name30')" />
 			</view>
 			<view class="input">
-				<input type="password" :placeholder="$t('app.name13')"  />
+				<input type="password" :placeholder="$t('app.name31')"  />
 			</view>
-			<view class="forget">
-				<text>{{$t("app.name14")}}</text>
+			<view class="input">
+				<input type="password" :placeholder="$t('app.name32')"  />
 			</view>
+		
 			<view class="btn">
 				<view class="">
-					<text>{{$t("app.name15")}}</text>
+					<text>{{$t("app.name33")}}</text>
 				</view>
 			</view>
 			<view class="create">
-				<text>{{$t("app.name16")}}</text>
+				<text>{{$t("app.name34")}}?</text>
+			</view>
+			<view class="ul">
+				<view class="title">
+					<text>{{$t("app.name35")}}</text>
+				</view>
+				<ul class="content">
+					<li class="item" v-for="(item,index) in ulList" :key="index">
+						
+					</li>
+				</ul>
+			</view>
+			<view class="other">
+				<view class="xieyi">
+					<checkbox-group @change="change">
+						<label>
+							<checkbox style="transform:scale(0.8)" value="cb" checked="true" />
+						</label>
+					</checkbox-group>
+					<view class="text">
+						<text>{{$t("app.name19")}} <text class="text1">《{{$t("app.name20")}}》</text>{{$t("app.name21")}}  <text class="text1">《{{$t("app.name22")}}》</text></text>
+					</view>
+				</view>
 			</view>
 		</view>
 	</view>
@@ -45,7 +68,18 @@
 			return {
 				
 			};
-		}
+		},
+		computed:{
+			ulList(){
+				return [
+					{name:this.$t("app.name36")},
+					{name:this.$t("app.name37")},
+					{name:this.$t("app.name38")},
+					{name:this.$t("app.name39")},
+					{name:this.$t("app.name40")},
+				]
+			}
+		},
 	}
 </script>
 
@@ -53,6 +87,20 @@
 	page{
 		height: 100%;
 		// background-color: red;
+	}
+	/deep/ .uni-checkbox-input-checked{
+		background-color: rgb(27, 171, 162);
+		border: none;
+	}
+	/deep/ .uni-checkbox-input{
+		box-sizing: border-box;
+		border-color: #969799 !important;
+	}
+	/deep/ .uni-checkbox-input:hover{
+		border-color: #969799 !important;
+	}
+	/deep/ .uni-checkbox-input.uni-checkbox-input-checked:before{
+		color: white;
 	}
 	.page-home{
 		width: 100%;
@@ -130,6 +178,38 @@
 				padding-top: 40rpx;
 				color: #41AF74;
 				font-size: 28rpx;
+			}
+			.other{
+				width: 95%;
+				margin: 0 auto;
+				// border-top: 1px solid #DCDFE6;
+				// border-bottom: 1px solid #DCDFE6;
+				box-sizing: border-box;
+				padding: 40rpx 0;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				// display: flex;
+				// flex-direction: row;
+				// align-items: center;
+				uni-checkbox-group{
+					display: flex;
+					align-items: center;
+					justify-content: center;
+					padding-top: 5rpx;
+					font-size: 26rpx;
+				}
+				.xieyi{
+					display: flex;
+					flex-direction: row;
+					align-items: center;
+					color: #777777;
+					font-size: 26rpx;
+					margin-bottom: 30rpx;
+					.text1{
+						color: #41AF74;
+					}
+				}
 			}
 		}
 	}
